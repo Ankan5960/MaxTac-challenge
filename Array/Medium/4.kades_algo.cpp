50a30c5a6ca0761b8b67c602278e54cf677b281b
ankan5960 <ankanmaity27@gmail.com>
2024-07-02 01:30:11 +0530

    kedeens alogorithm

---
+++ Array/Medium/4.kades_algo.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+#include<vector>
+#include<climits>
+
+using namespace std;
+class Solution{
+    public:
+        int maxSum_better(vector<int>& nums)
+        {
+            int maxSum=INT16_MIN;
+            int n=nums.size();
+            for(int i=0;i<n;i++)
+            {
+                int sum=0;
+                for(int j=i;j<n;j++)
+                {
+                    sum+=nums[j];
+                    maxSum=max(maxSum,sum);
+                }
+            }
+            return maxSum;
+        }
+
+        int maxSum_optimal(vector<int>& nums)
+        {
+            long long maxsum=LONG_MIN;
+            long long sum=0;
+            for(int i=0;i<nums.size();i++)
+            {
+                sum+=nums[i];
+                if(sum>maxsum)
+                {
+                    maxsum=sum;
+                }
+                if(sum<0)
+                {
+                    sum=0;
+                }
+            }
+            return maxsum;
+        }
+};
+
+int main()
+{
+    Solution sol;
+    vector<int>nums={-2,1,-3,4,-1,2,1,-5,4};
+    cout<<sol.maxSum_optimal(nums);
+}
