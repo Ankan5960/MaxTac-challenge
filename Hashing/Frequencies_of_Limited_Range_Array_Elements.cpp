95e6e97ca1a2abbce599c5ca5ac4feaa99272dc2
ankan5960 <ankanmaity27@gmail.com>
2024-07-18 13:59:20 +0530

    Hashing/Frequencies_of_Limited_Range_Array_Elements.cpp

---
+++ Hashing/Frequencies_of_Limited_Range_Array_Elements.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<vector>
+#include<map>
+
+using namespace std;
+void frequencyCount(vector<int>& arr,int N, int P)
+{ 
+        // map<int,int> mpp;
+        // for(int i=0;i<N;i++)
+        // {
+        //     mpp[arr[i]]++;
+        // }
+        // int j=0;
+        // for(int i=1;i<=N;i++)
+        // {
+        //     arr[j]=mpp[i];
+        //     j++;
+        // }
+        vector<int> mpp={0};
+        for(int i=0;i<N;i++)
+        {
+            mpp[arr[i]]++;
+        }
+        int j=0;
+        for(int i=1;i<=N;i++)
+        {
+            //arr[j]=mpp[i];
+            cout<<mpp[i]<<" ";
+        }
+}
+
+int main()
+{
+    vector<int> arr={2, 3, 2, 3, 5};
+    frequencyCount((arr),5,5);
+    // for(auto it:arr)
+    // {
+    //     cout<<it<<" ";
+    // }
+
+}
